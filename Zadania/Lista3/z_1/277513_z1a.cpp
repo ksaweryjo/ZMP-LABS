9f87ea5f73ee7e6d79e2910145fd134afe91921f
Hush <ksaweryjo@proton.me>
2024-03-25 18:47:38 +0100

    Fixed an oopsie

---
+++ Zadania/Lista3/z_1/277513_z1a.cpp
@@ -0,0 +1,63 @@
+// Ksawery Józefowski nr 277513 zadanie 1a
+
+#include <iostream>
+#include <cstdio>
+
+// "Przegrywa gracz, który jest zmuszony wziąć ostatnią zapałkę."
+void Game(int matches){
+    int p1, p2;
+    int temp = 0;
+    while (matches > 1) {
+        // Player 1
+        do {
+            // Jakaś informacja zwrotna przy złym ruchu
+            if(temp != 0){ // Chciałem to wrzucaćna stderr, ale jakoś się psuje wygląd
+                printf("Nielegalny Ruch, Powtorz\n\n");
+            }
+            printf("Ruch Gracza nr 1: ");
+            scanf("%d", &p1);
+            temp++;
+        } while (!(p1 >= 1 && p1 <= 3 && p1 <= matches));
+        temp = 0;
+
+        matches -= p1;
+        if (matches == 1) {
+            printf("Gracz nr 2 Przegral");
+            break;
+        }
+            // Jest opcja, żę zostanie 2-3 zapałek i ktoś je wszystkie weźmie
+        else if (matches < 1){
+            printf("Gracz nr 1 Przegral");
+            break;
+        }
+
+        // Player 2
+        do {
+            if(temp != 0){
+                printf("Nielegalny Ruch, Powtorz");
+            }
+            printf("Ruch Gracza nr 2: ");
+            scanf("%d", &p2);
+            temp++;
+        } while (!(p2 >= 1 && p2 <= 3 && p2 <= matches));
+        temp = 0;
+
+        matches -= p2;
+        if (matches == 1) {
+            printf("Gracz nr 1 Przegral");
+            break;
+        }
+        else if (matches < 1){
+            printf("Gracz nr 2 Przegral");
+            break;
+        }
+    }
+}
+
+int main() {
+    int matches;
+    printf("Podaj liczbe zapalek: ");
+    scanf("%d", &matches);
+    Game(matches);
+    return 0;
+}
